1ffc2e312a015b029c335604a16a86bd9daee9ec
Chris Bendel <chrissbendel@gmail.com>
2025-11-23 10:24:07 -0500

    Orders notes

--- app/controllers/storefront_controller.rb
+++ app/controllers/storefront_controller.rb
@@ -1,6 +1,7 @@
 class StorefrontController < ApplicationController
   def show
     @store = Store.find_by!(slug: params[:slug])
-    @orders = @store.orders.open
+    # TODO Wire in orders when we have them
+    # @orders = @store.orders.open
   end
 end
