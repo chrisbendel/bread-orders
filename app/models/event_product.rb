8acd1820f6964864819a8785c79501b031936118
Chris Bendel <chrissbendel@gmail.com>
2025-12-06 10:11:24 -0500

    Event products table

--- app/models/event_product.rb
+++ app/models/event_product.rb
@@ -5,4 +5,12 @@ class EventProduct < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :quantity, numericality: {greater_than_or_equal_to: 0}
   validates :price_cents, numericality: {greater_than_or_equal_to: 0}
+
+  def price
+    price_cents.to_f / 100
+  end
+
+  def price_formatted
+    "$%.2f" % price
+  end
 end
