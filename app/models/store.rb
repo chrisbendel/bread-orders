e9494db718a465948a9b2d4a1cb1943f05959754
Chris Bendel <chrissbendel@gmail.com>
2025-12-10 09:19:29 -0500

    Tests

--- app/models/store.rb
+++ app/models/store.rb
@@ -2,10 +2,21 @@ class Store < ApplicationRecord
   belongs_to :user
   has_many :events, dependent: :destroy
   has_many :notifications, class_name: "StoreNotification", dependent: :destroy
+  has_many :orders
 
   validates :name, presence: true
   validates :slug,
     presence: true,
     uniqueness: true,
     format: {with: /\A[a-z0-9-]+\z/i}
+
+  # eventually check `user.subscription_active?` when integrating payments
+  def monetization_allowed?
+    true
+  end
+
+  # Placeholder for when orders are implemented
+  def active_orders?
+    false
+  end
 end
