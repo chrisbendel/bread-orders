a022b8f74ecc5a8ff6e915684809855560e7bc3f
Chris Bendel <chrissbendel@gmail.com>
2025-12-10 08:53:00 -0500

    Flash dialog styling

--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -21,13 +21,13 @@ class DashboardControllerTest < ActionDispatch::IntegrationTest
     store = Store.create!(user: User.create!(email: "store@example.com"), name: "Bakery", slug: "bakery")
     event = store.events.create!(name: "Weekly Bake", orders_open_at: 1.day.ago, orders_close_at: 1.day.from_now, pickup_at: 2.days.from_now)
     product = event.event_products.create!(name: "Sourdough", price_cents: 1000, quantity: 10)
-    
+
     order = @user.orders.create!(event: event)
     order.order_items.create!(event_product: product, quantity: 1)
 
     sign_in_as(@user)
     get dashboard_path
-    
+
     assert_response :success
     assert_select "h4", "Your Orders"
     assert_select "td", "Bakery"
