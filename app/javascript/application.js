d21a337a03d329b4de1e6bdbf89dbe741956566d
Chris Bendel <chrissbendel@gmail.com>
2025-12-11 11:35:43 -0500

    Remove unused stimulus listener

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,12 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-
-// Global "Click Outside" behavior for <details> menus
-window.addEventListener("click", (event) => {
-    document.querySelectorAll("details[open]").forEach((details) => {
-        if (!details.contains(event.target)) {
-            details.removeAttribute("open");
-        }
-    });
-});
